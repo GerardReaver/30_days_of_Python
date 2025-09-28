238a0325466a49a9a752d0e44471adee2ed5c0e8
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-09-28 00:07:25 -0400

    Add files via upload
    This was Day1 of Day 30 COMPLETED!!!

---
+++ helloworld.py
@@ -0,0 +1,42 @@
+# Day 1 - 30 Days of Python Challenge
+
+print(2 + 3) # addition (+)
+print(3 - 1) # subtraction (-)
+print(2 * 3) # multiplication (*)
+print(3 / 2) # division (/)
+print(3 ** 2) # exponential (**)
+print(3 % 2) # modulus(%)
+print(3 // 2) # Floor division operator (//)
+
+# Chekcing data types
+print(type(10))  # int
+print(type(3.14)) # float
+print(type(1 + 3j)) # complex number
+print(type('Hello, World!')) # string
+print(type([1, 2, 3])) # List
+print(type({'name': 'Gerard'})) # Dictionary
+print(type({9.8, 3.14, 2.7})) # Set
+print(type((9.8, 3.14, 2.7))) # Tuple
+
+# Doing excercises for Day 1
+print(3 + 4) # addition
+print(3 - 4) # subtraction
+print(3 % 4) # modulus
+print(3 * 4) # multiplication
+print(3 / 4) # division
+print(3 ** 4) # exponential
+print(3 // 4) # Floor division
+print('Gerard')
+print('Ben')
+print('USA')
+print(' I am enjoying 30 days of python')
+print(type(10)) 
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Gerard', 'Python', 'Finland']))
+print(type('Gerard'))
+print(type('Ben'))
+print(type('USA'))
+
+# Day 1 of 30 Completed
